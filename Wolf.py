064f50339fffba3bce2afca573272d76ceffffa9
Ines Gabille <ines.gabille@etu.minesparis.psl.eu>
2026-01-30 13:32:28 +0100

    deplacement loup

--- Wolf.py
+++ Wolf.py
@@ -9,27 +9,27 @@ class Loup :
         self.energie = energie
         self.age = age
     
-    def deplacement(self, grille, n):
-        x,y = self.pos
-        voisins = [(x+1,y), (x-1, y), (x, y+1), (x, y-1)]
-        for p in voisins :
-            if (0 <= p[0] < c.GRID_SIZE) and (0 <= p[1] < c.GRID_SIZE) :
-                if grille[p[0], p[1], 1] == 1 :
-                    x,y = p
-                    #self.alimentation()
-            else :
-                valide = False
-                while not valide :
-                    new = rd.randint(0,3)
-                    newpos = voisins[new]
-                    nx, ny = newpos
-                    if grille[nx, ny, 2] == 0 :
-                        self.pos = newpos
-                        valide = True
-                    else :
-                        voisins.pop(new)
-                    if len(voisins) == 0:
-                        valide = True
+def deplacement(loup, grille):
+    x,y = loup.pos
+    voisins = [(x+1,y), (x-1, y), (x, y+1), (x, y-1)]
+    for p in voisins :
+        if (0 <= p[0] < c.GRID_SIZE) and (0 <= p[1] < c.GRID_SIZE) :
+            if grille[p[0], p[1], 1] == 1 :
+                x,y = p
+                #self.alimentation()
+        else :
+            valide = False
+            while not valide :
+                new = rd.randint(0,3)
+                newpos = voisins[new]
+                nx, ny = newpos
+                if grille[nx, ny, 2] == 0 :
+                    loup.pos = newpos
+                    valide = True
+                else :
+                    voisins.pop(new)
+                if len(voisins) == 0:
+                    valide = True
     
     #def alimentation(self):
     #    self.energie = self.energie + WOLF_ENERGY_FROM_SHEEP
