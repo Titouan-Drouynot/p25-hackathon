ba002a3b4e888a01ef775e88b6e7195fc755e591
Alexandre ROUGET <alexandre.rouget@etu.minesparis.psl.eu>
2026-01-30 14:55:02 +0100

    correction bord déplacement

--- Wolf.py
+++ Wolf.py
@@ -18,15 +18,17 @@ class Loup :
 def deplacement(loup, grille):
     x,y = loup.pos
     voisins = [(x+1,y), (x-1, y), (x, y+1), (x, y-1)]
+    for i in range(len(voisins)):
+        if (voisins[i, 0] < 0)or(voisins[i, 0] > c.GRID_SIZE-1)or(voisins[i, 1] < 0)or(voisins[i, 1] > c.GRID_SIZE-1):
+                    voisins.pop(i)
     for p in voisins :
-        if (0 <= p[0] < c.GRID_SIZE) and (0 <= p[1] < c.GRID_SIZE) :
-            if grille[p[0], p[1], 1] == 1 :
-                x,y = p
-                #self.alimentation()
+        if grille[p[0], p[1], 1] == 1 :
+            x,y = p
+            #self.alimentation()
         else :
             valide = False
             while not valide :
-                new = rd.randint(0,3)
+                new = rd.randint(0,len(voisins))
                 newpos = voisins[new]
                 nx, ny = newpos
                 if (0 <= nx < c.GRID_SIZE) and (0 <= ny < c.GRID_SIZE):
