9da31f50fb3b78ccbf47573b5a43fa9a4b1dd539
Ines Gabille <ines.gabille@etu.minesparis.psl.eu>
2026-01-30 11:23:33 +0100

    reproduction mouton

--- mouton.py
+++ mouton.py
@@ -1,3 +1,5 @@
+import random as rd
+
 class Mouton:
     def __init__(self, x, y, energie, age):
         self.pos = (x,y)
@@ -22,5 +24,22 @@ def gain_energie_mouton(grille, mouton):
 def reproduction_mouton(grille, mouton, dic_moutons):
     if mouton.energie > SHEEP_REPRODUCTION_THRESHOLD:
         NUMBER_SHEEP += 1
-        nouveau_mouton = Mouton(mouton.x, mouton.y, SHEEP_INITIAL_ENERGY, 0)
-        dic_moutons[NUMBER_SHEEP] = nouveau_mouton
+        voisins = [(x+1,y), (x-1, y), (x, y+1), (x, y-1)]
+        valide = False
+        while not valide :
+            new = rd.randint(0,3)
+            newpos = voisins[new]
+            nx, ny = newpos
+            if (grille[nx, ny, 2] == 0) and (grille[nx, ny, 1] == 0) :
+                valide = True
+            else :
+                voisins.pop(new)
+            if len(voisins) == 0:
+                valide = True
+                nx = -1
+        if nx != -1:
+            nouveau_mouton = Mouton(mouton.x, mouton.y, SHEEP_INITIAL_ENERGY, 0)
+            dic_moutons[NUMBER_SHEEP] = nouveau_mouton
+
+"""def mort_mouton(mouton):
+    if mouton.age > SHEEP_MAX_AGE :"""
